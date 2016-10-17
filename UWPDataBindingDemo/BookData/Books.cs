16ce425ca641cb0169c8904cc5fba5b6d6b23477
mike <mike@V-DOXIE-W10>
2016-10-17 15:39:36 +0800

    Add project files.

---
+++ UWPDataBindingDemo/BookData/Books.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UWPDataBindingDemo.BookData
+{
+    public class Book
+    {
+        public string ImageCover { get; set; }
+        public string Name { get; set; }
+        public string Author { get; set; }
+    }
+
+    public class BookCover
+    {
+        public string ImageCover { get; set; }
+    }
+    public class BookManager
+    {
+
+        public static ObservableCollection<Book> getBooks()
+        {
+            var bookList = new ObservableCollection<Book>();
+            bookList.Add(new Book { ImageCover = @"Assets/bookCovers/book1.jpg", Name = "ADAPTIVE WEB DESIGN", Author = "Aaron Gustafson" });
+            bookList.Add(new Book { ImageCover = @"Assets/bookCovers/book2.jpg", Name = "The Doctor In War", Author = "Woods Hutchinson" });
+            bookList.Add(new Book { ImageCover = @"Assets/bookCovers/book3.jpg", Name = "Mysterious", Author = "John Doe" });
+            bookList.Add(new Book { ImageCover = @"Assets/bookCovers/book4.jpg", Name = "Harry Potter", Author = "J.K. Rowling" });
+            bookList.Add(new Book { ImageCover = @"Assets/bookCovers/book5.jpg", Name = "The ICEBERG", Author = "Tove Jansson" });
+            bookList.Add(new Book { ImageCover = @"Assets/bookCovers/book6.jpg", Name = "The Cobra", Author = "Mike X X Ma" });
+            bookList.Add(new Book { ImageCover = @"Assets/bookCovers/book7.jpg", Name = "The MAZE Runner", Author = "James Dashner" });
+            bookList.Add(new Book { ImageCover = @"Assets/bookCovers/book8.jpg", Name = "1984", Author = "George Orwell" });
+            return bookList;
+        }
+        public static List<BookCover> getCovers()
+        {
+            var bookCovers = new List<BookCover>();
+            bookCovers.Add(new BookCover { ImageCover = @"Assets/bookCovers/book1.jpg" });
+            bookCovers.Add(new BookCover { ImageCover = @"Assets/bookCovers/book2.jpg" });
+            bookCovers.Add(new BookCover { ImageCover = @"Assets/bookCovers/book3.jpg" });
+            bookCovers.Add(new BookCover { ImageCover = @"Assets/bookCovers/book4.jpg" });
+            bookCovers.Add(new BookCover { ImageCover = @"Assets/bookCovers/book5.jpg" });
+            bookCovers.Add(new BookCover { ImageCover = @"Assets/bookCovers/book6.jpg" });
+            bookCovers.Add(new BookCover { ImageCover = @"Assets/bookCovers/book7.jpg" });
+            bookCovers.Add(new BookCover { ImageCover = @"Assets/bookCovers/book8.jpg" });
+            return bookCovers;
+        }
+
+    }
+}
---
+++ UWPDataBindingDemo/MainPage.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using UWPDataBindingDemo.BookData;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace UWPDataBindingDemo
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+
+    public sealed partial class MainPage : Page
+    {
+        private ObservableCollection<Book> Books; // Mainpage.xmal中 girdview 绑定的数据是必须名字一致"Books"
+        private List<BookCover> Covers;
+        public MainPage()
+        {
+            this.InitializeComponent();
+            Books = BookManager.getBooks();
+            Covers = BookManager.getCovers();
+        }
+
+        private void GridView_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            // 2个参数 sender 是点击的控件对象 e是控件对象绑定的对象
+            var book = (Book)e.ClickedItem;
+            SelectedBook.Text = "You selected " + book.Name;
+        }
+
+        private void AddBt_Click(object sender, RoutedEventArgs e)
+        {
+            String name = BookName.Text;
+            String auther = BookAuther.Text;
+
+            var img = (BookCover)BookCoverImg.SelectedItem;
+            string cover = img.ImageCover;
+            Book newBook = new Book();
+            //newBook.Author = auther;
+            //newBook.Name = name;
+            //newBook.ImageCover = cover;
+            //Books.Add(newBook);
+            Books.Add(new Book { Name = name, Author = auther, ImageCover = cover });
+        }
+
+        private void BookCover_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBox cb = (ComboBox)sender;
+
+        }
+    }
+}
